d50f1e38a4af857c9d5af9d6762939a1ca0581ef
angela-tf <angela11lux@gmail.com>
2025-11-08 20:07:35 +0000

    changed identification method bc of expired token

--- netlify/functions/send-email.mjs
+++ netlify/functions/send-email.mjs
@@ -62,11 +62,8 @@ export const handler = async (event) => {
     const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                type: "OAuth2",
                 user: process.env.EMAIL_USER,
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                refreshToken: process.env.REFRESH_TOKEN
+                pass: process.env.APP_PASSWORD
             },
         })
 
